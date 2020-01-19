ba5620e2ce140ac02fbfac8fd6ef2ee3db39a249
bwettenstein <wettenstein@gmail.com>
2020-01-18 18:00:15 -0800

    Made grid and grid hovering functional

--- main.js
+++ main.js
@@ -1,40 +1,24 @@
-let gridContainer = document.getElementById('container');
-let gridToAdd = document.createDocumentFragment('div');
-let newBoard = document.getElementById('resizeBoard');
-let clearBoard = document.getElementById('clearBoard');
-const col = document.getElementById('col');
+const gridContainer = document.querySelector('.container');
 
-// Need to ask the user this eventually 
-let column = 16;
-let row = 16;
-
-// Create the grid using user entered dimensions
-let createGrid = (column, row) => {
-    for (let v = 0; v < column; v++) {
+// FIGURE OUT HOW TO MAKE THE FIRST ROW disappear
+let createGrid = (size=16) => {
+    for (let v = 0; v < size; v++) {
         let column = document.createElement('div');
         //column.id = 'col' + (v+1);
-        column.id = 'col';
-        column.className = 'column'
+        column.id = 'containerGrid';
+        column.class = 'container-grid';
     
-        for(let g = 0; g<row; g++) {
+        for(let g = 0; g<size; g++) {
             let row = document.createElement('div');
             //row.id = 'row' + (g+1); 
-            row.id = 'row';
-            row.className = 'row';
+            row.id = 'grid';
+            row.className = 'grid';
             column.appendChild(row);
         }
         gridContainer.appendChild(column);
     }
 }
 
-let promptNewGrid = () => {
-    let newColumn = prompt("Enter new height for grid");
-    let newRow = prompt("Enter new width for grid");
-    createGrid(newColumn, newRow);
-}
-
-newBoard.addEventListener('click', promptNewGrid);
-createGrid(column, row);
-resetBoard();
+createGrid();
 
 
