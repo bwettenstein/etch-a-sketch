7305d231e2a22f458708a524485f2e24d476c727
bwettenstein <wettenstein@gmail.com>
2020-02-04 20:43:44 -0800

    Can change background color of grid

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const grid = document.querySelector('#grid-container');
 const clearBoardButton = document.querySelector('#clearBoard');
 const resizeBoardButton = document.querySelector('#resizeBoard')
-const chooseColor = document.querySelector('#chooseColor');
-
+const tile = document.getElementsByClassName('tile');
+const chooseGridBackgroundColor = document.querySelector('#chooseGridBackgroundColor');
 const defaultGridSize = 16;
-let gridColor = "red";
+const defaultGridColor = '#f1b0da';
+const clearGridColor = '#ffffff';
+
+clearBoardButton.addEventListener('click', clearBoard);
+resizeBoardButton.addEventListener('click', resizeBoard);
+chooseGridBackgroundColor.addEventListener('change', changeGridBackgroundColor, false);
+
+
+createGrid(defaultGridSize);
 
 function createGrid(gridSize) {
     // Making the column
@@ -17,25 +25,22 @@ function createGrid(gridSize) {
             let newRow = document.createElement('div');
             newRow.className = 'tile';
             newRow.id = 'row';
-            newRow.addEventListener("mouseover", changeColorOnHover)
+            newRow.addEventListener('mouseover', changeColorOnHover)
             newColumn.appendChild(newRow);
         }
         grid.appendChild(newColumn);
     }
 }
-createGrid(defaultGridSize);
 
-const tile = document.getElementsByClassName('tile');
-clearBoardButton.addEventListener("click", clearBoard);
-resizeBoardButton.addEventListener("click", resizeBoard);
+
 
 function changeColorOnHover() {
-    this.style.backgroundColor = gridColor;
+    this.style.backgroundColor = defaultGridColor;
 }
 
 function clearBoard() {
     for(let x = 0; x<tile.length; x++) {
-        tile[x].style.backgroundColor = 'white';
+        tile[x].style.backgroundColor = clearGridColor;
     }
 }
 // Used when resizing the grid
@@ -45,12 +50,12 @@ function deleteGrid() {
     }
 }
 function promptGridSize() {
-    let newGridSize = prompt("Enter new grid size", "16");
+    let newGridSize = prompt('Enter new grid size', defaultGridSize);
     if (newGridSize > 0) {
         return newGridSize;
     }
     else {
-        alert("Invalid response");
+        alert('Invalid response');
     }
 }
 function resizeBoard() {
@@ -61,3 +66,8 @@ function resizeBoard() {
     }
 }
 
+function changeGridBackgroundColor(event) {
+    for(let x = 0; x<tile.length; x++) {
+        tile[x].style.backgroundColor = event.target.value;
+    }
+}
